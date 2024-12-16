8724a9f4e3bf435ff215725198f6730b1463a6df
Camilo <jumellecamilo@gmail.com>
2024-12-16 17:15:04 +0100

    Cors actualizado usando el .env

--- server/src/Pool.ts
+++ server/src/Pool.ts
@@ -1,24 +1,4 @@
-import { Pool } from "pg";
 import 'dotenv/config';
-
-// const { DB_HOST, DB_USER, DB_PSW, DB_DATABASE, DB_PORT } = process.env;
-
-// const pool = new Pool({
-//     user: DB_USER,
-//     host: DB_HOST,
-//     database: DB_DATABASE,
-//     password: DB_PSW,
-//     port: Number(DB_PORT),
-// });
-
-// async function sendQuery(query = '', values?: any[]) {
-//     const results: any = await pool.query(query, values);
-
-//     return results;
-// }
-
-// export { sendQuery };
-
 
 
 import { drizzle } from 'drizzle-orm/node-postgres';
